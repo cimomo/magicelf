7a9bf07a2257b7d74c3a67785660823616b3250b
Kai Chen <kaichen@outlook.com>
2020-03-09 17:12:44 -0700

    Setting things up

---
+++ src/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello from Gin")
+	})
+
+	r.Run(":8080")
+}
