c655564af5b87ded7989ab4e05636ec48c5a0534
Kai Chen <kaichen@outlook.com>
2020-03-10 16:39:55 -0700

    Load stuff

--- src/main.go
+++ src/main.go
@@ -8,9 +8,14 @@ import (
 
 func main() {
 	r := gin.Default()
+
+	r.LoadHTMLGlob("templates/**/*.html")
+
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello from Gin")
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
+
+	r.Static("/public", "./public")
 
 	r.Run(":8080")
 }
